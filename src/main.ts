d06fabda72ffc003c6b4d2d58a234d6e092f9e38
Natalya2121 <andriyenkonatalya@gmail.com>
2024-05-09 12:40:47 +0300

    my homework

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+// import { appConfig } from './app/app.config';
+// import { Comp1Component } from './app/comp1/comp1.component';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+
+  // bootstrapApplication(Comp1Component, appConfig)
+  // .catch((err) => console.error(err));
